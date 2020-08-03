7251fc54e727febe9be17ec9a3b8b399ee8846ab
Radouane Bahi <radbahi@gmail.com>
2020-08-03 19:14:26 -0400

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"; // rememeber react router. powerful url tool.
+import "./App.css";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
---
+++ src/components/navbar.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from React
+import { Link } from "react-router-dom"
+
+export default class Navbar extends Component { // look at more recent ways to make components with past projects
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">ExcerTracker</Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">Exercises</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Create Exercise Log</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Create user</Link>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+        )
+    }
+}
